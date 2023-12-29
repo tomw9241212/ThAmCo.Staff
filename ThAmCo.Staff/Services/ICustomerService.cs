41cebb5ec1161dfba0afb843bd42544140ad2a1e
watchmychops <thomas_baxter92@hotmail.com>
2023-12-29 15:05:49 +0000

    View and anonymise customer data

--- ThAmCo.Staff/Services/ICustomerService.cs
+++ ThAmCo.Staff/Services/ICustomerService.cs
@@ -3,7 +3,8 @@
 namespace ThAmCo.Staff.Services {
     public interface ICustomerService {
         public Task<List<CustomerGetDto>> GetCustomersAsync();
-        public Task<CustomerGetDto> GetCustomerAsync(int id);
+        public Task<CustomerGetDto?> GetCustomerAsync(int id);
+        public Task<bool> DeleteCustomerAsync(int id);
 
     }
 }
