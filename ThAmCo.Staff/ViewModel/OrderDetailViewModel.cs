52733849b72b1e31b7a05dfecf48b64c9d1a821e
watchmychops <thomas_baxter92@hotmail.com>
2023-12-30 19:36:30 +0000

    Show order details

---
+++ ThAmCo.Staff/ViewModel/OrderDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ThAmCo.Staff.Models;
+
+namespace ThAmCo.Staff.ViewModel {
+    public class OrderDetailViewModel {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; } = null!;
+        public string CustomerEmail { get; set; } = null!;
+        public OrderStatus OrderStatus { get; set; }
+        public DateTime SubmittedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+    }
+}
