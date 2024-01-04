48a09bc1489f2cc68ac27eea38483f900728da9b
watchmychops <thomas_baxter92@hotmail.com>
2024-01-04 11:44:38 +0000

    Added tests on Orders Service

--- ThAmCo.Staff.Tests/OrdersServiceTests.cs
+++ ThAmCo.Staff.Tests/OrdersServiceTests.cs
@@ -64,6 +64,80 @@ namespace ThAmCo.Staff.Tests {
         }
 
         [Fact]
+        public async Task GetOrdersAsync_WithValid_ShouldOkEntities() {
+
+            // Arrange
+            var expectedResult = new List<OrderGetDto> {
+                new OrderGetDto {
+                    Id = 1,
+                    CustomerId = 1,
+                    SubmittedDate = DateTime.Now,
+                    OrderDetails = new List<OrderDetail> {
+                        new OrderDetail { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 0.05f }
+                    }
+                }
+            };
+            var expectedJson = JsonConvert.SerializeObject(expectedResult);
+            var httpMock = CreateHttpMock(HttpStatusCode.OK, expectedJson);
+            var tokenHttpMock = CreateTokenHttpMock();
+            var service = CreateOrdersService(httpMock, tokenHttpMock);
+
+            // Act
+            var result = await service.GetOrdersAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedResult.Count, result.Count);
+            Assert.Equal(expectedResult.First().Id, result.First().Id);
+            Assert.Equal(expectedResult.First().CustomerId, result.First().CustomerId);
+            Assert.Equal(expectedResult.First().SubmittedDate, result.First().SubmittedDate);
+            Assert.Equal(expectedResult.First().OrderDetails.First().ProductId, result.First().OrderDetails.First().ProductId);
+            Assert.Equal(expectedResult.First().OrderDetails.First().Quantity, result.First().OrderDetails.First().Quantity);
+            Assert.Equal(expectedResult.First().OrderDetails.First().UnitPrice, result.First().OrderDetails.First().UnitPrice);
+
+            httpMock.Protected().Verify(
+                    "SendAsync",
+                    Times.Once(),
+                    ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get
+                      && req.RequestUri == new Uri("https://localhost:7153/api/Orders")),
+                   ItExpr.IsAny<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task GetOrdersAsync_WithInvalid_ShouldThrowException() {
+            // Arrange
+            var expectedResult = new List<OrderGetDto> {
+                new OrderGetDto {
+                    Id = 1,
+                    CustomerId = 1,
+                    SubmittedDate = DateTime.Now,
+                    OrderDetails = new List<OrderDetail> {
+                        new OrderDetail { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 0.05f }
+                    }
+                }
+            };
+            var expectedJson = JsonConvert.SerializeObject(expectedResult);
+            var httpMock = CreateHttpMock(HttpStatusCode.BadRequest, expectedJson);
+            var tokenHttpMock = CreateTokenHttpMock();
+            var service = CreateOrdersService(httpMock, tokenHttpMock);
+
+            // Act
+            var result = await Assert.ThrowsAsync<HttpRequestException>(service.GetOrdersAsync);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Response status code does not indicate success: 400 (Bad Request).", result.Message);
+
+            httpMock.Protected().Verify(
+                "SendAsync",
+                Times.Once(),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get
+                    && req.RequestUri == new Uri("https://localhost:7153/api/Orders")),
+                ItExpr.IsAny<CancellationToken>());
+        }
+
+
+        [Fact]
         public async Task GetOrderAsync_WithValid_ShouldOkEntity() {
             // Arrange
             var expectedResult = new OrderGetDto {
@@ -101,6 +175,148 @@ namespace ThAmCo.Staff.Tests {
                     ItExpr.IsAny<CancellationToken>());
         }
 
+        // A test for GetOrderAsync with an invalid id
+        [Fact]
+        public async Task GetOrderAsync_WithInvalid_ShouldThrowException() {
+            // Arrange
+            var expectedResult = new OrderGetDto {
+                Id = 1,
+                CustomerId = 1,
+                SubmittedDate = DateTime.Now,
+                OrderDetails = new List<OrderDetail> {
+                    new OrderDetail { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 0.05f }
+                }
+            };
+            var expectedJson = JsonConvert.SerializeObject(expectedResult);
+            var httpMock = CreateHttpMock(HttpStatusCode.BadRequest, expectedJson);
+            var tokenHttpMock = CreateTokenHttpMock();
+            var service = CreateOrdersService(httpMock, tokenHttpMock);
+
+            // Act
+            var result = await Assert.ThrowsAsync<HttpRequestException>(() => service.GetOrderAsync(1));
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Response status code does not indicate success: 400 (Bad Request).", result.Message);
+
+            httpMock.Protected().Verify(
+                "SendAsync",
+                Times.Once(),
+                ItExpr.Is<HttpRequestMessage>(
+                        req => req.Method == HttpMethod.Get
+                        && req.RequestUri == new Uri("https://localhost:7153/api/Orders/1")),
+                ItExpr.IsAny<CancellationToken>());
+        }
+
+        // A test for GetOrderAsync with a non-existent id
+        [Fact]
+        public async Task GetOrderAsync_WithNonExistent_ShouldReturnNull() {
+            // Arrange
+            var expectedResult = new OrderGetDto {
+                Id = 1,
+                CustomerId = 1,
+                SubmittedDate = DateTime.Now,
+                OrderDetails = new List<OrderDetail> {
+                    new OrderDetail { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 0.05f }
+                }
+            };
+            var expectedJson = JsonConvert.SerializeObject(expectedResult);
+            var httpMock = CreateHttpMock(HttpStatusCode.NotFound, expectedJson);
+            var tokenHttpMock = CreateTokenHttpMock();
+            var service = CreateOrdersService(httpMock, tokenHttpMock);
+
+            // Act
+            var result = await service.GetOrderAsync(1);
+
+            // Assert
+            Assert.Null(result);
+
+            httpMock.Protected().Verify(
+                    "SendAsync",
+                    Times.Once(),
+                    ItExpr.Is<HttpRequestMessage>(
+                        req => req.Method == HttpMethod.Get
+                        && req.RequestUri == new Uri("https://localhost:7153/api/Orders/1")),
+                    ItExpr.IsAny<CancellationToken>());
+        }
+
+        // Test for GetOrdersByStatusAsync with a valid status
+        [Fact]
+        public async Task GetOrdersByStatusAsync_WithValid_ShouldOkEntities() {
+            // Arrange
+            var expectedResult = new List<OrderGetDto> {
+                new OrderGetDto {
+                    Id = 1,
+                    CustomerId = 1,
+                    Status = OrderStatus.Confirmed,
+                    SubmittedDate = DateTime.Now,
+                    OrderDetails = new List<OrderDetail> {
+                        new OrderDetail { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 0.05f }
+                    }
+                }
+            };
+            var expectedJson = JsonConvert.SerializeObject(expectedResult);
+            var httpMock = CreateHttpMock(HttpStatusCode.OK, expectedJson);
+            var tokenHttpMock = CreateTokenHttpMock();
+            var service = CreateOrdersService(httpMock, tokenHttpMock);
+
+            // Act
+            var result = await service.GetOrdersByStatusAsync(OrderStatus.Confirmed);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedResult.Count, result.Count);
+            Assert.Equal(expectedResult.First().Id, result.First().Id);
+            Assert.Equal(expectedResult.First().CustomerId, result.First().CustomerId);
+            Assert.Equal(expectedResult.First().SubmittedDate, result.First().SubmittedDate);
+            Assert.Equal(expectedResult.First().OrderDetails.First().ProductId, result.First().OrderDetails.First().ProductId);
+            Assert.Equal(expectedResult.First().OrderDetails.First().Quantity, result.First().OrderDetails.First().Quantity);
+            Assert.Equal(expectedResult.First().OrderDetails.First().UnitPrice, result.First().OrderDetails.First().UnitPrice);
+
+            httpMock.Protected().Verify(
+                    "SendAsync",
+                    Times.Once(),
+                    ItExpr.Is<HttpRequestMessage>(
+                        req => req.Method == HttpMethod.Get
+                        && req.RequestUri == new Uri("https://localhost:7153/api/Orders?orderStatus=Confirmed")),
+                    ItExpr.IsAny<CancellationToken>());
+        }
+
+        // Test a GetOrdersByStatusAsync with an invalid status
+        [Fact]
+        public async Task GetOrdersByStatusAsync_WithInvalid_ShouldThrowException() {
+            // Arrange
+            var expectedResult = new List<OrderGetDto> {
+                new OrderGetDto {
+                    Id = 1,
+                    CustomerId = 1,
+                    Status = OrderStatus.Confirmed,
+                    SubmittedDate = DateTime.Now,
+                    OrderDetails = new List<OrderDetail> {
+                        new OrderDetail { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 0.05f }
+                    }
+                }
+            };
+            var expectedJson = JsonConvert.SerializeObject(expectedResult);
+            var httpMock = CreateHttpMock(HttpStatusCode.BadRequest, expectedJson);
+            var tokenHttpMock = CreateTokenHttpMock();
+            var service = CreateOrdersService(httpMock, tokenHttpMock);
+
+            // Act
+            var result = await Assert.ThrowsAsync<HttpRequestException>(() => service.GetOrdersByStatusAsync(OrderStatus.Confirmed));
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Response status code does not indicate success: 400 (Bad Request).", result.Message);
+
+            httpMock.Protected().Verify(
+                    "SendAsync",
+                    Times.Once(),
+                    ItExpr.Is<HttpRequestMessage>(
+                               req => req.Method == HttpMethod.Get
+                               && req.RequestUri == new Uri("https://localhost:7153/api/Orders?orderStatus=Confirmed")),
+                    ItExpr.IsAny<CancellationToken>());
+        }
 
     }
 }
